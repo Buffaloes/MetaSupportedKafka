c90651fe51c706053a48c420ac59b0cc3ce078ab
BluesZhang <zhangjiehao@didichuxing.com>
2016-10-25 11:30:17 +0800

    增加日志

--- src/main/java/com/buffalo/kafka/KafkaEventProcessorRegister.java
+++ src/main/java/com/buffalo/kafka/KafkaEventProcessorRegister.java
@@ -1,10 +1,15 @@
 package com.buffalo.kafka;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Created by 张皆浩 on 16/10/20.
  * DIDI CORPORATION
  */
 public class KafkaEventProcessorRegister {
+
+    private static Logger LOGGER = LoggerFactory.getLogger(KafkaEventProcessorRegister.class);
 
     private static KafkaInboundEventProcessorGroup inboundGroup = new KafkaInboundEventProcessorGroup();
 
@@ -12,10 +17,12 @@ public class KafkaEventProcessorRegister {
 
     public static void registerInboundEventProcessor(KafkaInboundEventProcessor inbound) {
         inboundGroup.add(inbound);
+        LOGGER.info("Register inbound processor");
     }
 
     public static void registerOutboundEventProcessor(KafkaOutboundEventProcessor outbound) {
         outboundGroup.add(outbound);
+        LOGGER.info("Register outbound processor");
     }
 
     public static KafkaInboundEventProcessor getInbounds() {
